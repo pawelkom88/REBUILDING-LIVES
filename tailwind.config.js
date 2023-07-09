f3c016bf2243ca269ff879efe6d0d8bf94a7fb1b
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-07-09 10:04:02 +0100

    add styles, add dropdown library, add css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary-clr": "var(--primary-clr)",
+        "secondary-clr": "var(--secondary-clr)"
+      },
     },
   },
   plugins: [],
